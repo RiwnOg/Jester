8a8b5eb15d271e121bc61c4f81817b3ccb9d7a06
RiwnOg <126474223+RiwnOg@users.noreply.github.com>
2023-04-25 11:17:18 -0300

    feat: fateIn animation on navLoading

--- src/app/components/navbar/NavLoading.tsx
+++ src/app/components/navbar/NavLoading.tsx
@@ -1,12 +1,14 @@
 export default function NavLoading() {
   return (
-    <div className='fixed z-10 h-[66px] w-full bg-white shadow-sm dark:bg-stone-950'>
-      <div className='mx-auto max-w-[2520px] px-5 py-4 sm:px-2 md:px-10 xl:px-20'>
-        <div className='flex animate-pulse flex-row items-center justify-between space-x-4'>
-          <div className='h-10 w-10 rounded-full bg-slate-300 dark:bg-slate-700' />
-          <div className='flex flex-row items-center justify-between gap-3'>
-            <div className='hidden h-7 w-24 rounded bg-slate-300 dark:bg-slate-700 md:block' />
-            <div className='h-10 w-10 rounded-full bg-slate-300 dark:bg-slate-700' />
+    <div className="fixed z-10 h-[66px] w-full bg-white shadow-sm dark:bg-stone-950">
+      <div className="mx-auto max-w-[2520px] px-5 py-4 sm:px-2 md:px-10 xl:px-20">
+        <div className="animate-fade">
+          <div className="flex animate-pulse flex-row items-center justify-between space-x-4 ">
+            <div className="h-10 w-10 rounded-full bg-slate-300 dark:bg-slate-700" />
+            <div className="flex flex-row items-center justify-between gap-3">
+              <div className="hidden h-7 w-24 rounded bg-slate-300 dark:bg-slate-700 md:block" />
+              <div className="h-10 w-10 rounded-full bg-slate-300 dark:bg-slate-700" />
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       animation: {
-        vote: 'vote 0.5s ease-in-out',
+        vote: "vote 0.5s ease-in-out",
+        fade: "fadeIn 1s ease-in",
       },
       keyframes: {
         vote: {
-          '0%': {
-            transform: 'rotate(0deg)',
+          "0%": {
+            transform: "rotate(0deg)",
           },
-          '100%': {
-            transform: 'rotate(360deg)',
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        fadeIn: {
+          "0%": {
+            opacity: "0",
+            // transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            // transform: "translateY(0)",
           },
         },
       },
